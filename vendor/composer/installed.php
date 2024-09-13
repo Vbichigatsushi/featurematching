ce641b5d85f7b89e3bc8a34027c649753fd2d170
lucasvavon <lucasvavon@icloud.com>
2024-09-13 12:47:44 +0200

    fix: add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pimentbleu/featurematching',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '5532e8f229b64997daaf89f7a6cb83fc2ab3fc66',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pimentbleu/featurematching' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '5532e8f229b64997daaf89f7a6cb83fc2ab3fc66',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
